7431f5b158c061fd1e15226d0c05fe1699addca1
crabtr26 <crabtr26@gmail.com>
2022-05-13 21:47:20 -0400

    Adding poetry pyproject.toml

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-with open("src/mypackage/__version__.py", "r", encoding="UTF-8") as f:
-    text = f.read()
-    __version__ = text.split('"')[1]
-
-setup(version=__version__)
