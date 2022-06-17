bf055d60d20b711139af968965d82924d5fa3c13
crabtr26 <crabtr26@gmail.com>
2022-06-16 23:38:45 -0400

    Separating routes

--- src/pytemplates/app.py
+++ src/pytemplates/app.py
@@ -1,36 +1,14 @@
-import os
-import socket
+from fastapi import FastAPI
 
-import fastapi
+from pytemplates.routes import all_routes
 
-from pytemplates.core.module1 import greet
-from pytemplates.core.module2 import wish_farewell
-
-app = fastapi.FastAPI()
+app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return greet(user="PyTemplates User")
-
-
-@app.get("/hello")
-async def hello(user: str):
-    return greet(user=user)
-
-
-@app.get("/goodbye")
-async def hello(user: str):
-    return wish_farewell(user=user)
-
-
-@app.get("/whoami")
-async def whoami():
-    return {
-        "host_name": socket.gethostname(),
-        "host_ip": socket.gethostbyname(socket.gethostname()),
-        "process_id": os.getpid(),
-    }
+app.include_router(
+    all_routes.router,
+    prefix="",
+)
 
 
 if __name__ == "__main__":
---
+++ src/pytemplates/routes/all_routes.py
@@ -0,0 +1,33 @@
+import os
+import socket
+
+from fastapi import APIRouter
+
+from pytemplates.core.module1 import greet
+from pytemplates.core.module2 import wish_farewell
+
+router = APIRouter()
+
+
+@router.get("/")
+async def root():
+    return greet(user="PyTemplates User")
+
+
+@router.get("/hello")
+async def hello(user: str):
+    return greet(user=user)
+
+
+@router.get("/goodbye")
+async def hello(user: str):
+    return wish_farewell(user=user)
+
+
+@router.get("/whoami")
+async def whoami():
+    return {
+        "host_name": socket.gethostname(),
+        "host_ip": socket.gethostbyname(socket.gethostname()),
+        "process_id": os.getpid(),
+    }
