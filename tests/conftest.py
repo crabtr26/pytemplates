8d3502905b4b3be4e90397d1816115c940f66f85
crabtr26 <crabtr26@gmail.com>
2022-06-17 14:56:48 -0400

    Updating conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,24 @@
+import pytest
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def pytest_setup_options():
-    options = Options()
-    options.add_argument("--disable-gpu")
+@pytest.fixture(autouse=True)
+def setup():
+    chrome_service = Service(ChromeDriverManager().install())
+
+    chrome_options = Options()
+    options = [
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1200",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
     return options
