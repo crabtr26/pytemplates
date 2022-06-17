894db6e60cc9997c9aae2b74a14f732789606493
crabtr26 <crabtr26@gmail.com>
2022-06-16 22:02:56 -0400

    Removing tests

--- tests/test_module2.py
+++
@@ -1,6 +0,0 @@
-from pytemplates.core.module2 import wish_farewell
-
-
-def test_module2():
-    goodbye = wish_farewell(user="Jacob")
-    assert goodbye == "Goodbye Jacob!"
--- tests/test_performance.py
+++
@@ -1,7 +0,0 @@
-from locust import FastHttpUser, task
-
-
-class PyTemplatesUser(FastHttpUser):
-    @task
-    def test(self):
-        self.client.get("test")
